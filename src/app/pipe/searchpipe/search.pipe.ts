f39d13f3a57d67b67c458bbb11c575bdb003e49c
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-22 17:54:24 +0530

    modified : correction on syntax

--- src/app/pipe/searchpipe/search.pipe.ts
+++ src/app/pipe/searchpipe/search.pipe.ts
@@ -1,17 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'search'
+  name: 'searchPipe'
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(dataList:any[], search:string): unknown {
+  transform(dataList:any[], search:string): any[] {
     if(!dataList)
       return [];
-    if(!search)
-      return dataList;
-    search=search.toLowerCase();
-    return dataList.filter((data)=>{if(data.title.toLowerCase.include(search)|| data.description.toLowerCase.include(search))}) 
+  if(!search){
+    return dataList;
   }
+  search=search.toLowerCase();
+  return dataList.filter((data)=>data.name.toLowerCase().includes(search)|| data.description.toLowerCase().includes(search)) 
 
+  }
 }
