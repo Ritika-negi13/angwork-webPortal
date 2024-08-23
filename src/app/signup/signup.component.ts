21204767d615727ffd168e96adc2a068d5f97293
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-23 14:51:19 +0530

    added : functiom to add user on submitting details

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpService } from '../service/http/http.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,7 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
-name!:string;
-email!:string;
-password!:string;
+  name!:string;
+  email!:string;
+  password!:string;
+  constructor(private http:HttpService,private router:Router){}
+  addData(){
+    if(this.name!=='' && this.email!=='' && this.password!=='')
+    {
+      console.log("signup")
+      this.http.addnewuser(this.name,this.email,this.password).subscribe({
+        next:(res:any)=>{
+          console.log(res);
+          this.router.navigate(['/login']);
+        },
+        error:(err)=>{
+          console.log("err",err);
+        }
+      }
+      );
+    }
+  }
 }
