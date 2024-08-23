634a0271819e2da1cf82c1722df27dab5bc9651e
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-23 14:53:01 +0530

    modified : added new services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './dashboard/header/header.component';
+import {  MatSelectModule } from '@angular/material/select';
+import { ProductsComponent } from './dashboard/products/products.component';
+import {MatCardModule} from '@angular/material/card'
+import { HttpClientModule } from '@angular/common/http'; 
+import { HttpService } from './service/http/http.service';
+import { SearchPipe } from './pipe/searchpipe/search.pipe';
+import { ShowitemComponent } from './showitem/showitem.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    HeaderComponent,
+    ProductsComponent,
+    SearchPipe,
+    ShowitemComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
