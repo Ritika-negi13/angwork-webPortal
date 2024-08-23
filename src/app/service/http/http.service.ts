8e6b6f02065fd1aaa3535ddd0ca414e4221acefd
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-23 14:49:34 +0530

    added : function to get user data to login and add new user

--- src/app/service/http/http.service.ts
+++ src/app/service/http/http.service.ts
@@ -10,4 +10,19 @@ export class HttpService {
   getData(){
     return this.http.get("http://localhost:3000/product")
   }
+  editData(review:string,data:any){
+    data.review.push(review);
+    return this.http.patch("http://localhost:3000/product/"+data.id,data);
+  }
+  addnewuser(name:string,email:string,password:string){
+    const data={
+      "name":name,
+      "email":email,
+      "password":password
+    }
+    return this.http.post("http://localhost:3000/user",data)
+  }
+  getuserdata(){
+    return this.http.get("http://localhost:3000/user");
+  }
 }
