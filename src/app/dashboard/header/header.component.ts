e458e97be645c799d476b3f6cceac933a5d4d681
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-23 14:45:14 +0530

    add :css to dashboard

--- src/app/dashboard/header/header.component.ts
+++ src/app/dashboard/header/header.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { FilterService } from 'src/app/service/filter.service';
+import { SearchService } from 'src/app/service/search/search.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  search!:string;
+  constructor(private s:SearchService,private filterService:FilterService){}
+  searchData()
+  {
+    this.s.updateSearch(this.search);
+  }
+  onFilterChange() {
+    const filters = {
+      price: (document.getElementById('price') as HTMLSelectElement).value,
+      delivery: (document.getElementById('delivery') as HTMLSelectElement).value
+    };
+    this.filterService.setFilters(filters);
+  }
 }
