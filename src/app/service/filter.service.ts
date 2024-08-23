1f0c13bad5b7e75c4ead05d38edfc62d173ed248
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-23 14:48:39 +0530

    added : service to get data from header filters and update it to products

---
+++ src/app/service/filter.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterService {
+  private filtersSubject = new BehaviorSubject<any>({});
+  filters$ = this.filtersSubject.asObservable();
+
+  setFilters(filters: any) {
+    this.filtersSubject.next(filters);
+  }
+}
