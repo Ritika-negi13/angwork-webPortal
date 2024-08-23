e985800295262831e27cda70d3e4fd88309a4894
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-23 14:47:19 +0530

    added : function to check login details

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpService } from '../service/http/http.service';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-email!:string;
-password!:string;
+  email:string='';
+  password:string='';
+  data!:[
+    {
+      "name":string,
+      "email":string,
+      "password":string,
+    }
+  ];
+  constructor(private router:Router,private http:HttpService){}
+  loginuser(){
+    if(this.email!='' && this.password!='')
+    {
+
+      this.http.getuserdata().subscribe({
+        next:(res:any)=>{
+          this.data=res;
+          this.checkLogin();
+
+        },
+        error:(err)=>{
+          console.log("err",err);
+        }
+      })
+      this.router.navigate(['/dashboard'])
+    }
+  }
+  checkLogin(){
+    this.data.forEach(element => {
+      if(element.email===this.email && element.password===this.password)
+      {
+        this.router.navigate(['/dashboard']);
+      }
+    });
+  }
 }
